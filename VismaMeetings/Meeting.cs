04f2577ab59b09f35f6a65633370d0bb14753cd2
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2022-04-11 15:09:09 +0300

    update_kk

--- VismaMeetings/Meeting.cs
+++ VismaMeetings/Meeting.cs
@@ -2,49 +2,57 @@
 {
   public class Meeting
   {
-   public string Name { get; set; }
+    public string Name { get; set; }
+
+    public Meeting CurrentMeeting { get; private set; }
     public ResponsiblePerson ResponsiblePerson { get; set; }
     public string Description { get; set; }
 
     public List<string> Category = new()
     {
-      "CodeMonkey", "Hub", "Short", "TeamBuilding"
+      "CodeMonkey",
+      "Hub",
+      "Short",
+      "TeamBuilding"
     };
-    
+
     public List<string> Type = new()
     {
-          "Live", "InPerson"
+      "Live",
+      "InPerson"
     };
     public DateOnly StartDate;
     public DateOnly EndDate;
 
     private List<ResponsiblePerson> _persons = new();
-    private List<Meeting> _meetings = new();
 
     public void AddToMeeting(ResponsiblePerson person)
     {
+
+
       if (!_persons.Contains(person))
       {
         _persons.Add(person);
+
       }
       else
       {
         Console.WriteLine($"{person.Name} already in meeting!");
       }
+
     }
 
     public void RemoveFromMeeting(ResponsiblePerson responsiblePerson)
     {
-      if(_persons != null)
+      if (_persons != null)
         _persons.Remove(responsiblePerson);
     }
-   
-    public List<Meeting> GetAllMeetings() { return _meetings; }
+
+    public string MeetingInfo()
+    {
+      return Name + " " + Description + " " + ResponsiblePerson.Name;
+    }
+
 
   }
-
-
- 
-
-
 }
--- VismaMeetings/Program.cs
+++ VismaMeetings/Program.cs
@@ -1,22 +1,111 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using VismaMeetings;
 
+var meetings = new List<Meeting>();
 
 Meeting meeting = new()
 {
   Name = "C# conference",
-  Description = "",
-
+  Description = "Most popular",
+  ResponsiblePerson = new()
+  {
+    Name = "Petras Petraitis"
+  }
+};
+Meeting meeting2 = new()
+{
+  Name = "Java conference",
+  Description = "Best ever",
+  ResponsiblePerson = new()
+  {
+    Name = "Ona Onaitiene"
+  }
 };
 
-meeting.AddToMeeting(new ResponsiblePerson() { 
+meetings.Add(meeting);
+meetings.Add(meeting2);
 
-  Name ="Petras Petraitis"
+for (int i = 0; i < meetings.Count; i++)
+{
+  Console.WriteLine($"{i+1}. {meetings[i].Name}, {meetings[i].ResponsiblePerson.Name}");
+}
+/*Console.WriteLine("===============================");
+Console.WriteLine("=======Filter by ==========");
+Console.WriteLine("===============================");
+Console.WriteLine("1. Description.");
+Console.WriteLine("2. Responsible person.");
+Console.WriteLine("3. Category.");*/
 
-});
+Console.Write("Filter value: ");
+string filter = Console.ReadLine();
+var filterResult = meetings.Where(x => x.Description.ToLower() == filter.ToLower());
+  
+foreach(var ii in filterResult) { 
+  Console.WriteLine($"{ ii.MeetingInfo()}");
+}
 
 
-Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+
+
+int result = 1;
+
+while (result != 0)
+{
+ 
+  Console.WriteLine("===============================");
+  Console.WriteLine("=======VISMA Meetings==========");
+  Console.WriteLine("===============================");
+
+  Console.WriteLine("1. Create meeting.");
+  Console.WriteLine("2. Delete meeting.");
+  Console.WriteLine("3. Add Person to meeting.");
+  Console.WriteLine("4. Remove Person from meeting.");
+  Console.WriteLine("5. Display all meetings.");
+  Console.WriteLine("0. Close and Exit.");
+
+  string option = Console.ReadLine();
+  int.TryParse(option, out result);
+
+
+
+
+
+
+  switch (result)
+  {
+    case 1:
+      Console.WriteLine("1. Create meeting.");
+      break;
+      case 2:
+      Console.WriteLine("2. Delete meeting.");
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+      case 5:
+
+      break;
+    case 0:
+      break;
+    default:
+    
+      Console.WriteLine("Wrong input no option selected!");
+      break;
+  }
+}
+
+Console.WriteLine("Goodbye my friend :)");
 
 
 
